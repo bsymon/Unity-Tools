1df401ca6ac952c96868c4e3a5f12a234eb321fd
BENJAMIN-PC\Benjamin <benjamin@mail.com>
2017-09-28 23:44:01 +0200

    The real first commit

---
+++ Grid.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Game.World {
+
+[System.Serializable]
+public class Grid {
+	
+	public int width;
+	public int height;
+	public float _tileSize  = 1f;
+	public bool _middleTile = false;
+	public bool _2D         = false;
+	
+	// -- //
+	
+	public Vector3 GetWorldPosition(Vector2 gridPosition) {
+		Vector3 position = Vector3.zero;
+		float middle     = _middleTile ? _tileSize / 2f : 0f;
+		
+		position.x            = _tileSize * (int) gridPosition.x + middle;
+		position[_2D ? 1 : 2] = _tileSize * (int) gridPosition.y + middle;
+		
+		return position;
+	}
+	
+	public Vector2 GetGridPosition(Vector3 worldPosition) {
+		Vector2 position = Vector2.zero;
+		
+		position.x = Mathf.Floor(worldPosition.x / _tileSize);
+		position.y = Mathf.Floor(worldPosition[_2D ? 1 : 2] / _tileSize);
+		
+		return position;
+	}
+	
+	public Vector2 From1DTo2D(int i) {
+		return new Vector2(i % width, i / width);
+	}
+	
+	public int From2DTo1D(Vector2 v) {
+		return (int) (v.x + width * v.y);
+	}
+	
+}
+
+}
---
+++ ObjectPool.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Tools {
+
+public class ObjectPool<T> where T : Component {
+	
+	GameObject prefab;
+	T component;
+	
+	List<T> pool;
+	int poolSize;
+	int currentPoolIndex;
+	
+	// -- //
+	
+	public ObjectPool(GameObject prefab, int poolSize) {
+		this.prefab    = prefab;
+		this.component = prefab.GetComponent<T>();
+		this.poolSize  = poolSize;
+		this.pool      = new List<T>();
+		
+		// Create the object
+		prefab.SetActive(false);
+		
+		for(int i = 0; i < poolSize; ++ i) {
+			this.pool.Add(GameObject.Instantiate<T>(this.component));
+		}
+	}
+	
+	// -- //
+	
+	public T Dequeue() {
+		if(currentPoolIndex == poolSize - 1) {
+			// Scale up the pool
+			pool.Add(GameObject.Instantiate<T>(component));
+			poolSize++;
+		}
+		
+		if(currentPoolIndex < 0) {
+			currentPoolIndex = 0;
+		}
+		
+		T toDequeue = pool[currentPoolIndex];
+		currentPoolIndex++;
+		
+		toDequeue.gameObject.SetActive(true);
+		
+		return toDequeue;
+	}
+	
+	public T Dequeue(Vector3 position, Quaternion rotation) {
+		T toDequeue = Dequeue();
+		
+		toDequeue.transform.position = position;
+		toDequeue.transform.rotation = rotation;
+		
+		return toDequeue;
+	}
+	
+	public void Enqueue(T toEnqueue) {
+		if(!pool.Contains(toEnqueue)) {
+			// Add the object to the pool if it wasn't
+			pool.Add(toEnqueue);
+			poolSize++;
+			
+		}
+		
+		toEnqueue.gameObject.SetActive(false);
+		currentPoolIndex--;
+	}
+	
+	public void EnqueueAll() {
+		for(; currentPoolIndex > 0;) {
+			Enqueue(pool[currentPoolIndex - 1]);
+		}
+	}
+	
+}
+
+}
