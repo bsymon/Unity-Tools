66670a9cd67ed5266f24eba09e2ffc69266bddfa
BENJAMIN-PC\Benjamin <benjamin@mail.com>
2017-10-15 15:42:46 +0200

    Add SceneField

---
+++ Scene field for Inspector/Editor/SceneFieldDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(SceneField))]
+public class SceneFieldDrawer : PropertyDrawer {
+	
+	override public void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		SerializedProperty sceneAsset = property.FindPropertyRelative("sceneAsset");
+		SerializedProperty sceneName  = property.FindPropertyRelative("sceneName");
+		
+		EditorGUI.BeginChangeCheck();
+		
+		sceneAsset.objectReferenceValue = EditorGUI.ObjectField(position, label, sceneAsset.objectReferenceValue, typeof(SceneAsset), false);
+		
+		if(EditorGUI.EndChangeCheck()) {
+			if(sceneAsset.objectReferenceValue != null) {
+				sceneName.stringValue = ((SceneAsset) sceneAsset.objectReferenceValue).name;
+			} else {
+				sceneName.stringValue = "";
+			}
+		}
+	}
+	
+}
---
+++ Scene field for Inspector/SceneField.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public class SceneField {
+	
+	public string SceneName {
+		get { return sceneName; }
+	}
+	
+	// -- //
+	
+	[SerializeField]
+	private Object sceneAsset;
+	
+	[SerializeField]
+	private string sceneName;
+	
+	// -- //
+	
+	static public implicit operator string(SceneField sceneField) {
+		return sceneField.SceneName;
+	}
+	
+#if UNITY_EDITOR
+	static public implicit operator SceneAsset(SceneField sceneField) {
+		return sceneField.sceneAsset as SceneAsset;
+	}
+#endif
+	
+}
